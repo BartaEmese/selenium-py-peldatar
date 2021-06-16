973fcb29e9fbb79067998dcee6754ddbb923b1c6
BartaEmese <bagine.barta.emese@gmail.com>
2021-06-16 17:57:38 +0200

    for ciklus HF

--- testproject/python-homework/bartender.py
+++ testproject/python-homework/bartender.py
@@ -1,2 +1,20 @@
-age = int(input("Hány éves vagy?"))
-print(input())
+age = int(input("Hány éves vagy? "))
+drink = input("Mit szeretnél inni?: ")
+
+if drink == "kóla":
+    if age <= 60:
+        print("Parancsoljon a kólája!")
+    elif age > 60:
+        print("A koffein megemeli a vérnyomást")
+
+
+elif drink == "sör":
+    if age >= 18:
+        print("Parancsoljon a söre!")
+    elif age < 18:
+        print("Sajnos nem szolgálhatjuk ki!")
+else:
+    print("Csak sört és kólát tudunk adni")
+
+
+
