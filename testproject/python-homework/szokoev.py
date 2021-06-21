13be5ffefe309633da87cc189f3c85ead8614bc2
BartaEmese <bagine.barta.emese@gmail.com>
2021-06-21 11:22:07 +0200

    szokoev.py  hf

--- testproject/python-homework/szokoev.py
+++ testproject/python-homework/szokoev.py
@@ -1,1 +1,12 @@
-str
+# Még nincs kész.
+
+def szokoev_kal(x):
+    if x % 4 == 0 and x % 400 == 0:
+        return bool(x)
+
+evszam = int(input("Írj be egy évszámot: "))
+szokoev = szokoev_kal(evszam)
+if szokoev == "True":
+    print("Szökőév.")
+else:
+    print("Nem szökőév.")
