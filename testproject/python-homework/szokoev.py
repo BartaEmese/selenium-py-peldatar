9954aef02813f255156c30cc86c4f0e6b1d15471
BartaEmese <bagine.barta.emese@gmail.com>
2021-06-22 11:41:25 +0200

    hf modositas

--- testproject/python-homework/szokoev.py
+++ testproject/python-homework/szokoev.py
@@ -1,12 +1,30 @@
 # Még nincs kész.
 
-def szokoev_kal(x):
-    if x % 4 == 0 and x % 400 == 0:
-        return bool(x)
+def szokoev_kal(evszam):
+    if evszam % 400 == 0:
+        print("szökőév")
+    elif evszam % 100 == 0:
+        print("nem szökőév")
+    elif evszam % 4 == 0:
+        print("szökőév")
+    else:
+        print("nem szökőév")
 
+my_evszam = int(input("Írj be egy évszámot: "))
+print(szokoev_kal(my_evszam))
+
+
+# Ezt készítettem el először
 evszam = int(input("Írj be egy évszámot: "))
-szokoev = szokoev_kal(evszam)
-if szokoev == "True":
-    print("Szökőév.")
+if evszam % 400 == 0:
+     print("Szökőév")
+elif evszam % 100 == 0:
+    print("Nem szökőév")
+elif evszam % 4 == 0:
+    print("Szökőév")
 else:
-    print("Nem szökőév.")
+    print("Nem szökőév")
+
+
+
+
